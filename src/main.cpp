720ea0f59bdb6d2829021910727024bf9f3f7b52
gavinswilson@gmail.com <gavinswilson@gmail.com>
2023-11-23 08:44:44 -0800

    sync up commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "logging.h"
-
+#include "config.h"
 
 int main(){
     
     std::cout << "Starting System" << std::endl;
-
+d
 
     logging log;
     //log.write_to_main_log();
