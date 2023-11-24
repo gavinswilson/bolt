18ff184ed918e695aca1a0f468decda3b908b239
gavinswilson@gmail.com <gavinswilson@gmail.com>
2023-11-23 20:39:02 -0800

    added comments

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -9,7 +9,7 @@ class sudoku
         unsigned short int soln[9][9];
         unsigned short int boxes[9][9];
 
-        sudoku(int gridsize)
+        sudoku(int gridsize) //constructor
         {
             size=gridsize;
             reset_options();
@@ -19,7 +19,7 @@ class sudoku
             //print_arrays(2);
         }
 
-        int set_boxes(std::string type)
+        int set_boxes(std::string type) // set box structure - only standard supported so far
         {
             if (type == "std")
             {
@@ -41,7 +41,7 @@ class sudoku
             return 0;
         }
 
-        int reset_options()
+        int reset_options() // set all options back to default (511 for 9 cell grid with 9 optins)
         {
             int x,y;
             for (x=0;x<size; x++)
@@ -54,7 +54,7 @@ class sudoku
             return 0;
         }
 
-        int set_knowns(std::string list)
+        int set_knowns(std::string list) // load entire grid from a string - no checking yet
         {
             int listlen = list.length();
             int x,y,i;
@@ -72,14 +72,14 @@ class sudoku
             return 0;
         }
         
-        int set_knowns(int r, int c, int val)
+        int set_knowns(int r, int c, int val) // set a specific value at a location
         {
             soln[r-1][c-1] = val;
             print_arrays(1);
             return 0;
         }
 
-        int remove_all_known_options()
+        int remove_all_known_options() //scan entire grid for knowns and remove those options from the options array
         {
             int x,y,p,q,b,k;
             for (x=0; x<size; x++)
@@ -111,13 +111,13 @@ class sudoku
             return 0;
         }
         
-        int remove_option(int row, int col, int opt)
+        int remove_option(int row, int col, int opt) // remove specific value from list of options
         {
             options[row][col] = options[row][col]^(1<<(opt-1));
             return 0;
         }
 
-        int print_arrays(int choice)
+        int print_arrays(int choice)  // so many choices - needs to be to log file...
         {
             int x,y;
             std::cout << "---------------" << std::endl;
